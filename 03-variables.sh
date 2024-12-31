55af117f249b0c9e9ff39ffd96f939069579085e
pavan8909 <saipavan8909@gmail.com>
2024-12-31 06:59:10 +0530

    conversation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Suresh
+Person2=Ramesh
+
+echo "$Person1:: hi $Person2"
+echo "$Person2: hello $Person1"
+echo "$Perons1:: How are you doing"
+echo "$Person2:: i am doing good"
