d82d7af6296b990c8ee969d4feaf22abb79790de
pavan8909 <saipavan8909@gmail.com>
2025-01-02 06:46:31 +0530

    shell-script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,7 +3,7 @@
 MOVIES=("Pushpa" "rrr" "devara")
 
 echo "First movie: ${MOVIES[0]}"
-ech "second movie: ${MOVIES[1]}"
+echo "second movie: ${MOVIES[1]}"
 echo "third movie: ${Movies[2]}"
 
 echo "all movies are: ${Movies[@]}"
