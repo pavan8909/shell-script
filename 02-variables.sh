d0b5f962a91c042b4c4d63746ae980e82f979942
pavan8909 <saipavan8909@gmail.com>
2025-01-02 06:14:18 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,4 +2,5 @@
 
 echo "Rahim:: hi Robert"
 echo "Robert: hello Rahim"
-
+echo "Rahim:: How are you doing"
+echo "Robert:: i am doing good"
---
+++ 06-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Number1=$1
+NUmber2=$2
+
+
+TIMESTAMP=$(date)
+echo "script executed at: $TIMESTAMP"
+
+sum=($(Number1+NUmber2))
+
+echo "sum of $NUMBERS1 and $NUMBERS2 is : $sum"
