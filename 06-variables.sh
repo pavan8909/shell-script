8abc1f9bd0b1e135d7893d030bd36131c9a0c4e0
pavan8909 <saipavan8909@gmail.com>
2025-01-02 06:28:18 +0530

    shell-script

--- 06-variables.sh
+++ 06-variables.sh
@@ -5,7 +5,6 @@ NUMBER2=$2
 
 TIMESTAMP=$(date)
 echo "script executed at: $TIMESTAMP"
-
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "sum of $NUMBERS1 and $NUMBERS2 is : $sum"
+echo "sum of $NUMBER1 and $NUMBER2 is : $SUM"
