116ad46ad1fcc41238c7aeebd3dcd32a105bac3c
pavan8909 <saipavan8909@gmail.com>
2025-01-11 19:42:04 +0530

    conversation

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,5 +5,5 @@ Person2=$2
 
 echo "$Person1:: hi $Person2"
 echo "$Person2: hello $Person1"
-echo "$Perons1:: How are you doing"
+echo "$Person1:: How are you doing"
 echo "$Person2:: i am doing good"
---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hi $PERSON2"
+echo "$Person2: hello $PERSON2"
+echo "$PERSON1:: How are you doing"
+echo "$PERSON2:: i am doing good"
+
+echo "Please enter your username::"
+
+read USER
