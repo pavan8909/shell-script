7735154993eecd8acdb71c69a328bbb7c9706c97
pavan8909 <saipavan8909@gmail.com>
2025-01-12 00:18:42 +0530

    conditions

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,7 +8,12 @@ echo "ERROR :: you must have sudo access to execute this script"
 exit 1 #other than zero
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
 dnf install mysql -y
+
 if [ $? -ne 0  ]
 then
 echo "Installing Mysql ...Failure"
@@ -17,6 +22,13 @@ else
 echo "Installing Mysql ... Success"
 fi
 
+else
+
+echo "Mysql already ..installed"
+fi
+dnf list installed git
+if [ $? -ne 0  ]
+then
 dnf install git -y
 if [ $? -ne 0  ]
 then
@@ -24,6 +36,9 @@ echo "Installing git ...Failure"
 exit 1
 else
 echo "Installing git ... Success"
+fi
+else 
+echo "Installing git ... installed"
 fi
 
 
